25d3c9830902bea825ddc41c2a13d09dc6deed7b
Wesley Swart <wesley.swrt@gmail.com>
2020-01-20 12:05:00 +0200

    Modified pager adapter for 1 fragment

--- app/src/main/java/com/example/musicalstructureproject/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/musicalstructureproject/ui/main/SectionsPagerAdapter.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.musicalstructureproject.Fragment1;
 import com.example.musicalstructureproject.R;
 
 /**
@@ -17,7 +18,7 @@ import com.example.musicalstructureproject.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -27,9 +28,13 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new Fragment1();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
@@ -40,7 +45,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 1 total pages.
+        return 1;
     }
 }
