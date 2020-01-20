c5756a73fb1e6b9da3a24b8862d5e829054fc8d6
Wesley Swart <wesley.swrt@gmail.com>
2020-01-20 11:55:49 +0200

    Added Fragment1 java and xml files

--- app/src/main/java/com/example/musicalstructureproject/MainActivity.java
+++ app/src/main/java/com/example/musicalstructureproject/MainActivity.java
@@ -26,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        
+
     }
 }
