ca5ce48df4360faba0c7fa67fb328b8f87d509e2
Wesley Swart <wesley.swrt@gmail.com>
2020-03-09 08:56:39 +0200

    Added disclaimer and sound clip attributions

--- app/src/main/java/com/example/musicalstructureproject/Fragment1.java
+++ app/src/main/java/com/example/musicalstructureproject/Fragment1.java
@@ -1,3 +1,22 @@
+/*
+ * **DISCLAIMER**
+ * I (Wesley Swart) do not own any of the sound clips or images which appear
+ * in this application which is to be used for academic purposes only.
+ *
+ * All images downloaded from https://unsplash.com/
+ *
+ * All sound clips downloaded from https://freesound.org/
+ *
+ * Sound clips courtesy of the following artists/users:
+ *
+ * - Joe Mattson (Flick3r) : https://freesound.org/people/Flick3r/
+ * - Benjamin Mastripolito (Lemoncreme) :  https://benpm.github.io/
+ * - Setuniman : https://freesound.org/people/Setuniman/
+ * - JPMusic82 : https://soundcloud.com/jani-official/
+ * - Milton Paredes (milton.) : https://freesound.org/people/milton./
+ * - AlienXXX : https://freesound.org/people/AlienXXX/
+ */
+
 package com.example.musicalstructureproject;
 
 import android.content.Context;
